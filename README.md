365578f89d7d61e849a21e8fdb951292f12b5890
Carlos Mecha <mecha.carlos@gmail.com>
2015-06-12 09:40:46 -0700

    Fix issues and logging.

--- README.md
+++ README.md
@@ -4,20 +4,34 @@
 
 Carlos Mecha, 2015
 
-## Version v1
+## Version 1
 
-Small HTTP server to push and pull notifications.
+Developed form 06/15/2015 and released on 06/19/2015.
+
+Small HTTP server to push and retrieve notifications.
 
 ## Routes
 
 - `GET /:topic?`: Retrieves notifications by topic (all if topic is not set). The route accepts a
-query parameter `limit` with limit of returned records.
+query parameter `limit` with limit of returned records. Also, `requeue` could be specified and the retrieved messages won't
+be deleted.
 
 - `POST /:topic?`: Pushes a notification to the server. If the topic is not set, the default one will be assigned.
 The body defines the payload of the notification.
 
 ## Configuration
+Check `config.json`.
+
 By default, the server listen the port 3000.
+
+Before start, remember to run:
+
+```bash
+npm install
+```
+
+## Tests
+TODO (for v2).
 
 ## Scripts
 - `conf.sh`: Host and port configurations.
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var winston = require('winston');
 var util = require('util');
 var app = express();
 var mq = require('./mq');
+
+var config = require('./config');
+
+var transports = [];
+if(config.log.persistent) {
+    transports.push(new winston.transports.File({
+            filename: config.log.filename,
+            level: config.log.level
+    }));
+} else {
+    transports.push(new winston.transports.Console({
+            level: config.log.level
+    }));
+}
+
+var logger = new winston.Logger({transports: transports});
 
 var queues = null;
 var server = null;
@@ -11,53 +28,76 @@ var server = null;
 app.use(bodyParser.json());
 
 app.get('/:topic?', function(req, res){
+    logger.debug('Received GET %s', req.originalUrl);
     var topic = req.params.topic;
     var limit = req.query.limit || 1;
     var requeue = (req.query.requeue !== undefined);
+
+    logger.debug('Translated to GET /%s?limit=%d&requeue=%s', topic, limit, requeue);
+
     queues.get(topic, limit, requeue, function(err, results){
         if(err){
+            logger.error(err);
             res.status(500).json({error: err});
         } else {
+            logger.debug('Returned from %s, %d results: %j', req.originalUrl, results.length, results, {});
             res.json(results);
         }
     });
 });
 
 app.post('/:topic?', function(req, res){
+    logger.debug('Received POST %s', req.originalUrl);
+    logger.debug('Body %j', req.body, {});
     var topic = req.params.topic || 'default';
     queues.push(topic, req.body, function(err){
         if(err){
+            logger.error(err);
             res.status(500).json({error: err});
         } else {
+            logger.debug('Returned OK from %s', req.originalUrl);
             res.json({code: 'OK'});
         }
     });
 });
 
 function shutdown() {
-    console.log('Shutting down the server.');
+    logger.info('Shutting down the server.');
     if(queues != null){
+        logger.debug('Shutting down mq service.');
         queues.close();
     }
     if(server != null){
+        logger.debug('Shutting down http server.');
         server.close();
     }
+}
+
+function init(err, mqService) {
+    if(err) {
+        logger.error(err);
+        return;
+    }
+
+    queues = mqService;
+    server = app.listen(config.port, config.host, function(){
+        var host = server.address().address;
+        var port = server.address().port;
+        logger.info('Server listening at http://%s:%s', host, port);
+    });
+
 }
 
 process.on('SIGTERM', shutdown);
 process.on('SIGINT', shutdown);
 
-mq(function(err, q) {
-    if(err) {
-        console.error(err);
-        return;
-    }
-
-    queues = q;
-    server = app.listen(3000, 'localhost', function(){
-        var host = server.address().address;
-        var port = server.address().port;
-        console.log('Server listening at http://%s:%s', host, port);
-    });
-});
+// Init
+logger.debug('Initializing the server.');
+if(config.db.persistent) {
+    logger.debug('Set persistent mq service.');
+    mq(config.db.filename, init);
+} else {
+    logger.debug('Set efemeral mq service.');
+    mq(init);
+}
 
