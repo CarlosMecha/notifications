5b8d0a3540c3fb164231f19c0dba81dc591e8cfa
Carlos Mecha <mecha.carlos@gmail.com>
2015-06-17 10:09:46 -0700

    add mocha and chai frameworks.

---
+++ test/get.js
@@ -0,0 +1,13 @@
+
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+
+describe('Get a notification', function(){
+    describe('Requeue', function() {
+        it('Doesn\'t delete notifications', function(){
+            assert.ok(true, "Everything OK");
+        });
+    });
+});
+
